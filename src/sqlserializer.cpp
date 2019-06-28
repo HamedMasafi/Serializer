13e9da1ac5113d934cfea5f07c820a60a7049a63
Hamed Masafi <hamed.masafi@gmail.com>
2019-06-29 00:58:02 +0430

    SqlSerializer escape bug fixed

--- src/sqlserializer.cpp
+++ src/sqlserializer.cpp
@@ -13,70 +13,74 @@ SqlSerializer::SqlSerializer() :StringSerializer ()
         break;
 QString SqlSerializer::escapeString(const QString &str) const
 {
-    QString ret;
-    for (int i = 0; i < str.length(); ++i) {
-        switch (str.at(i).cell()) {
-        CASE_W('\\', "\\\\")
-        CASE_W('\r', "\\r")
-        CASE_W('\n', "\\n")
-        CASE_W('\a', "\\a")
-        CASE_W('\b', "\\b")
-        CASE_W('\f', "\\f")
-        CASE_W('\'', "''")
-        CASE_W('\t', "\\t")
-        CASE_W('\v', "\\v")
-//        CASE_W('\"', "\\\"")
+//    QString ret;
+//    for (int i = 0; i < str.length(); ++i) {
+//        switch (str.at(i).cell()) {
+//        CASE_W('\\', "\\\\")
+//        CASE_W('\r', "\\r")
+//        CASE_W('\n', "\\n")
+//        CASE_W('\a', "\\a")
+//        CASE_W('\b', "\\b")
+//        CASE_W('\f', "\\f")
+//        CASE_W('\'', "''")
+//        CASE_W('\t', "\\t")
+//        CASE_W('\v', "\\v")
+////        CASE_W('\"', "\\\"")
 
-        default:
-            ret.append(str.at(i));
-        }
-    }
-    return ret;
+//        default:
+//            ret.append(str.at(i));
+//        }
+//    }
+//    return ret;
+    QString ret(str);
+    return ret.replace("'", "''");
 }
 
 QString SqlSerializer::unescapeString(const QString &str) const
 {
-    QString ret;
-    for (int i = 0; i < str.length(); ++i) {
-        if (str.at(i) == '\'' && str.at(i + 1) == '\'' && str.length() > i + 1) {
-            ret.append('\'');
-            i++;
-            continue;
-        }
-        if (str.at(i) == '\\' && str.length() > i) {
-            switch (str.at(++i).cell()) {
-            case '\\':
-                ret.append("\\");
-                break;
-            case 'r':
-                ret.append("\r");
-                break;
-            case 'n':
-                ret.append("\n");
-                break;
-            case 'a':
-                ret.append("\a");
-                break;
-            case 'b':
-                ret.append("b");
-                break;
-            case 'f':
-                ret.append("\f");
-                break;
-            case 't':
-                ret.append("\t");
-                break;
-            case 'v':
-                ret.append("\v");
-                break;
+//    QString ret;
+//    for (int i = 0; i < str.length(); ++i) {
+//        if (str.at(i) == '\'' && str.at(i + 1) == '\'' && str.length() > i + 1) {
+//            ret.append('\'');
+//            i++;
+//            continue;
+//        }
+//        if (str.at(i) == '\\' && str.length() > i) {
+//            switch (str.at(++i).cell()) {
+//            case '\\':
+//                ret.append("\\");
+//                break;
+//            case 'r':
+//                ret.append("\r");
+//                break;
+//            case 'n':
+//                ret.append("\n");
+//                break;
+//            case 'a':
+//                ret.append("\a");
+//                break;
+//            case 'b':
+//                ret.append("b");
+//                break;
+//            case 'f':
+//                ret.append("\f");
+//                break;
+//            case 't':
+//                ret.append("\t");
+//                break;
+//            case 'v':
+//                ret.append("\v");
+//                break;
 
-            default:
-                ret.append(str.at(i));
-            }
-        } else {
-            ret.append(str.at(i));
-        }
-    }
+//            default:
+//                ret.append(str.at(i));
+//            }
+//        } else {
+//            ret.append(str.at(i));
+//        }
+//    }
 
-    return ret;
+//    return ret;
+    QString ret(str);
+    return ret.replace("''", "'");
 }
